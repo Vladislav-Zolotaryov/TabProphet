b4b25f7e566e4d69314ea532bd84fb2eecf634af
Vladislav Zolotaryov <zolotaryov.v.g@gmail.com>
2015-04-03 23:40:29 +0300

    Better event handling, working prototype. Bugged multi-window usage. Url does not update if you change url location in selected tab.

--- TabProphet/lib/main.js
+++ TabProphet/lib/main.js
@@ -1,46 +1,81 @@
+var { modelFor } = require("sdk/model/core");
+var { viewFor } = require("sdk/view/core");
+var { Style } = require('sdk/stylesheet/style');
+
 var tabs = require("sdk/tabs");
 var clipboard = require("sdk/clipboard");
 var windowUtils = require("sdk/window/utils");
+var { attachTo, detachFrom } = require('sdk/content/mod');
 
 var shiftPressed = false;
 var selectedTabs = [];
+var shiftKeyCode = 16;
 
-function onOpen(tab) {
-	console.log(tab.url + " is open");
-	tab.on("activate", logActivate);
+function regsiterOnTabClickEvent(tab) {
+  var lowLevelTab = viewFor(tab);
+  var clickClojure = function(tab) { 
+	return function() {
+		onTabClick(tab);
+	}
+  }
+  lowLevelTab.addEventListener("click", clickClojure(tab));
 }
 
-function logActivate(tab) {
+for (let tab of tabs) {
+	regsiterOnTabClickEvent(tab);
+}
+tabs.on("open", regsiterOnTabClickEvent);
+
+function onTabClick(tab) {
 	console.log(tab.url + " is activated");
 	if (shiftPressed) {
-		selectedTabs.push(tab);
-		
-		tab.style.color = 'red';
-		
-		console.log("Added " + Object.keys(tab));
-		var cliptext = '';
-		for (i = 0; i < selectedTabs.length; i++) {
-			cliptext += selectedTabs[i].url + '\n';
+		var tabView = viewFor(tab);
+		var index = selectedTabs.indexOf(tab);
+		if (index > -1) {
+			selectedTabs.splice(index, 1);
+			tabView.style.color = tabView.style.previousColor;
+		} else {
+			selectedTabs.push(tab);
+			tabView.style.previousColor = tabView.style.color;
+			tabView.style.color = 'blue';
 		}
-		clipboard.set(cliptext);
+		flushToClipboard();
 	}
 }
-tabs.on("open", onOpen);
 
+function flushToClipboard() {
+	var cliptext = '';
+	for (i = 0; i < selectedTabs.length; i++) {
+		cliptext += selectedTabs[i].url + '\n';
+	}
+	clipboard.set(cliptext);
+}
 
 function onKeyUp(event) {
-	console.log(event.keyCode);	
-	if (event.keyCode == 16) {
+	console.log("KEYUP " + event.keyCode);	
+	if (event.keyCode == shiftKeyCode) {
 		shiftPressed = false;
 	}
 }
 
 function onKeyDown(event) {
-	console.log(event.keyCode);	
-	if (event.keyCode == 16) {
+	console.log("KEYDOWN " + event.keyCode);	
+	if (event.keyCode == shiftKeyCode) {
 		shiftPressed = true;
 	}
 }
 
-windowUtils.windows()[0].addEventListener("keyup", onKeyUp, true);
-windowUtils.windows()[0].addEventListener("keydown", onKeyDown, true);
+var allWindows = windowUtils.windows(null, {includePrivate:true});
+for (let window of allWindows) {
+	attachKeyListeners(window);
+}
+
+allWindows.on('open', function(window) {
+	console.log("Opened new window " + window);
+	attachKeyListeners(window);
+});
+
+function attachKeyListeners(window) {
+	window.addEventListener("keyup", onKeyUp, true);
+	window.addEventListener("keydown", onKeyDown, true);
+}
